f793aab1076a9a58bcf3872cd3f1c153a7664811
Ritchy <iamrichardcn@gmail.com>
2024-06-02 00:04:44 +0100

    first set up commit

---
+++ data/raw/firstfile.py
@@ -0,0 +1,6 @@
+
+import pandas as pd
+
+file_path = "\\Users\\Dell\\Documents\\premium\\victor Innocent\\New topic\\ML-Web-APP-energy-efficiency-prediction\\cleaned_housing_data.csv"
+df = pd.read_csv(file_path)
+df.head()
