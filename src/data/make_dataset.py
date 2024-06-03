c4d150db597b1c3794a6fed876653e853d87eeb1
Ritchy <iamrichardcn@gmail.com>
2024-06-03 16:34:35 +0100

    defining the function to read data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -16,6 +16,8 @@ len(file)
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------f 
+
+# Base data path
 data_path = '../../data/raw/MetaMotion\\'
 
 f = file[0]
@@ -39,6 +41,7 @@ gyr_df=pd.DataFrame()
 acc_set=1
 gyr_set=1
 
+# Process each file
 for f in file:
     participant= f.split('-')[0].replace(data_path, '')
     Label= f.split('-')[1]
@@ -52,20 +55,43 @@ for f in file:
     df["category"] = category
         
     if "Accelerometer" in f:
+        df["set"]=acc_set
+        acc_set +=1
         acc_df=pd.concat([acc_df, df])
     
     if "Gyroscope" in f:
+        df["set"]=gyr_set
+        gyr_set +=1
         gyr_df=pd.concat([gyr_df, df])
     
-
+#acc_df[acc_df["set"]==1]
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+gyr_df.info()
+
+pd.to_datetime(df["epoch (ms)"], unit="ms")
+
+acc_df.index=pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index=pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
+
 
 
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+file = glob('../../data/raw/MetaMotion/*.csv')
+data_path = '../../data/raw/MetaMotion\\'
+
+def read_data_from_files(file)
 
 
 # --------------------------------------------------------------
