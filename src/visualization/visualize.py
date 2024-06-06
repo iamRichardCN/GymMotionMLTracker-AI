a8df374f072b51dc4697aaeba8f547c5290e531b
Ritchy <iamrichardcn@gmail.com>
2024-06-06 15:57:37 +0100

    Plot multiple axis

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -33,22 +33,38 @@ mpl.rcParams["figure.figsize"] = (20, 5) # Set the figure size to (20, 5) inches
 mpl.rcParams["figure.dpi"]= 100 # Set the figure DPI (dots per inch) to 100
 
 
-
-
-
 # --------------------------------------------------------------
 # Compare medium vs. heavy sets
 # --------------------------------------------------------------
+category_df=df.query("Label =='squat'").query("participant== 'A'").reset_index(drop=True)
 
+fig, ax=plt.subplots()
+category_df.groupby("category")["Gyr_y"].plot()
+ax.set_ylabel("Gyr_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 # --------------------------------------------------------------
 # Compare participants
 # --------------------------------------------------------------
+participant_df=df.query("Label =='bench'").sort_values("participant").reset_index(drop=True)
 
+fig, ax=plt.subplots()
+participant_df.groupby("participant")["Gyr_y"].plot()
+ax.set_ylabel("Gyr_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 # --------------------------------------------------------------
 # Plot multiple axis
 # --------------------------------------------------------------
+
+# Define the label
+label = "squat"
+# Define the participant
+participant = "A"
+# Query the DataFrame for rows where the label is 'squat' and the participant is 'A'
+All_axis_df = df.query(f"Label == '{label}'").query(f"participant == '{participant}'").reset_index(drop=True)
 
 
 # --------------------------------------------------------------
