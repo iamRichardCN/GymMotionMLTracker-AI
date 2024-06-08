1b1a937775edfb54cf710aa617402bd09cb97ed4
Ritchy <iamrichardcn@gmail.com>
2024-06-08 18:53:48 +0100

    Chauvenets criterion

--- src/features/removeoutliers.py
+++ src/features/removeoutliers.py
@@ -18,7 +18,7 @@ plt.rcParams["figure.figsize"] = (20, 5) # Set the figure size to (20, 5) inches
 plt.rcParams["figure.dpi"]= 100 # Set the figure DPI (dots per inch) to 100
 
 df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 10))
-df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 5))
+df[["Gyr_x", "Label"]].boxplot(by="Label", figsize=(20, 5))
 plt.show()
 
 df[outlier_columns[:3] + ["Label"]].boxplot(by="Label", figsize=(20, 5), layout=(1,3))
@@ -114,14 +114,68 @@ plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"_outlier", reset
 
 #
 # Loop over all columns
+for col in outlier_columns:
+    dataset=mark_outliers_iqr(df, col)
+    plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"_outlier", reset_index=True)
 
 
 
 # --------------------------------------------------------------
 # Chauvenets criteron (distribution based)
 # --------------------------------------------------------------
-
 # Check for normal distribution
+
+df[["Acc_x", "Label"]].plot.hist(by="Label", figsize=(20, 20))
+df[["Gyr_x", "Label"]].plot.hist(by="Label", figsize=(20, 20))
+plt.show()
+
+df[outlier_columns[:3] + ["Label"]].plot.hist(by="Label", figsize=(20, 20), layout=(3,3))
+df[outlier_columns[3:] + ["Label"]].plot.hist(by="Label", figsize=(20, 20), layout=(3,3))
+plt.show()
+
+def mark_outliers_chauvenet(dataset, col, C=2):
+    """Finds outliers in the specified column of datatable and adds a binary column with
+    the same name extended with '_outlier' that expresses the result per data point.
+    
+    Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/Chapter3/OutlierDetection.py
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+        C (int, optional): Degree of certainty for the identification of outliers given the assumption 
+                           of a normal distribution, typicaly between 1 - 10. Defaults to 2.
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column 
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+    # Compute the mean and standard deviation.
+    mean = dataset[col].mean()
+    std = dataset[col].std()
+    N = len(dataset.index)
+    criterion = 1.0 / (C * N)
+
+    # Consider the deviation for the data points.
+    deviation = abs(dataset[col] - mean) / std
+
+    # Express the upper and lower bounds.
+    low = -deviation / math.sqrt(C)
+    high = deviation / math.sqrt(C)
+    prob = []
+    mask = []
+
+    # Pass all rows in the dataset.
+    for i in range(0, len(dataset.index)):
+        # Determine the probability of observing the point
+        prob.append(
+            1.0 - 0.5 * (scipy.special.erf(high[i]) - scipy.special.erf(low[i]))
+        )
+        # And mark as an outlier when the probability is below our criterion.
+        mask.append(prob[i] < criterion)
+    dataset[col + "_outlier"] = mask
+    return dataset
 
 
 # Insert Chauvenet's function
