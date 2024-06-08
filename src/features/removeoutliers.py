a426253ecd524610f97d7a912dc66dada9cc0d09
Ritchy <iamrichardcn@gmail.com>
2024-06-08 18:22:20 +0100

    Insert IQR function

--- src/features/removeoutliers.py
+++ src/features/removeoutliers.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
 # Load data
 # --------------------------------------------------------------
 df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
-
+outlier_columns= list(df.columns[:6])
 # --------------------------------------------------------------
 # Plotting outliers
 # --------------------------------------------------------------
@@ -21,6 +21,57 @@ df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 10))
 df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 5))
 plt.show()
 
+df[outlier_columns[:3] + ["Label"]].boxplot(by="Label", figsize=(20, 5), layout=(1,3))
+df[outlier_columns[3:] + ["Label"]].boxplot(by="Label", figsize=(20, 5), layout=(1,3))
+plt.show()
+
+
+def plot_binary_outliers(dataset, col, outlier_col, reset_index):
+    """ Plot outliers in case of a binary outlier score. Here, the col specifies the real data
+    column and outlier_col the columns with a binary value (outlier or not).
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): Column that you want to plot
+        outlier_col (string): Outlier column marked with true/false
+        reset_index (bool): whether to reset the index for plotting
+    """
+
+    # Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/util/VisualizeDataset.py
+
+    dataset = dataset.dropna(axis=0, subset=[col, outlier_col])
+    dataset[outlier_col] = dataset[outlier_col].astype("bool")
+
+    if reset_index:
+        dataset = dataset.reset_index()
+
+    fig, ax = plt.subplots()
+
+    plt.xlabel("samples")
+    plt.ylabel("value")
+
+    # Plot non outliers in default color
+    ax.plot(
+        dataset.index[~dataset[outlier_col]],
+        dataset[col][~dataset[outlier_col]],
+        "+",
+    )
+    # Plot data points that are outliers in red
+    ax.plot(
+        dataset.index[dataset[outlier_col]],
+        dataset[col][dataset[outlier_col]],
+        "r+",
+    )
+
+    plt.legend(
+        ["outlier " + col, "no outlier " + col],
+        loc="upper center",
+        ncol=2,
+        fancybox=True,
+        shadow=True,
+    )
+    plt.show()
+
 
 
 # --------------------------------------------------------------
@@ -29,7 +80,32 @@ plt.show()
 
 # Insert IQR function
 
+def mark_outliers_iqr(dataset, col):
+    """Function to mark values as outliers using the IQR method.
 
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column 
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+
+    Q1 = dataset[col].quantile(0.25)
+    Q3 = dataset[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    dataset[col + "_outlier"] = (dataset[col] < lower_bound) | (
+        dataset[col] > upper_bound
+    )
+
+    return dataset
 # Plot a single column
 
 
