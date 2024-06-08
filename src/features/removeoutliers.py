409043340e43c36839d5b6c5d1e31cc36275cbd5
Ritchy <iamrichardcn@gmail.com>
2024-06-08 17:50:23 +0100

    outliers plotting

--- src/features/removeoutliers.py
+++ src/features/removeoutliers.py
@@ -8,11 +8,19 @@ from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
 # --------------------------------------------------------------
 # Load data
 # --------------------------------------------------------------
-
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 
 # --------------------------------------------------------------
 # Plotting outliers
 # --------------------------------------------------------------
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5) # Set the figure size to (20, 5) inches
+plt.rcParams["figure.dpi"]= 100 # Set the figure DPI (dots per inch) to 100
+
+df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 10))
+df[["Acc_x", "Label"]].boxplot(by="Label", figsize=(20, 5))
+plt.show()
+
 
 
 # --------------------------------------------------------------
